2bac89dedc7c91a2c222ddcf96a56477b1782f99
schoolonnie <lsmith19@byupathway.edu>
2026-01-14 09:40:51 -0600

    week02 Learning Activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Resume resume = new Resume();
+        resume._name = "John Doe";
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Tech Corp";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Senior Developer";
+        job2._company = "Innovatech";
+        job2._startYear = 2021;
+        job2._endYear = 2024;
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
