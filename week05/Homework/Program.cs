146ef3cdfaacc1cd2efb841caa4e8b9b31a01446
schoolonnie <lsmith19@byupathway.edu>
2026-02-02 09:40:15 -0600

    week05 Learning Activities

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Darius Arius", "Algebra", "5.1", "1-10");
+        WritingAssignment writingAssignment = new WritingAssignment("Joan Jones", "Literature", "The Great Gatsby Essay");
+
+        Console.WriteLine("Math Assignment Summary:");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        Console.WriteLine();
+
+        Console.WriteLine("Writing Assignment Summary:");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
