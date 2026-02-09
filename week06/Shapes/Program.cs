1b8865cda9bbf9e50a1580ee29d49a99c709b4d0
schoolonnie <lsmith19@byupathway.edu>
2026-02-09 10:42:02 -0600

    polymorphism learning activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,20 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square1 = new Square("green", 5);
+        Rectangle rect1 = new Rectangle("blue", 11.2, 10);
+        Circle cir1 = new Circle("red", 6);
+        
+        List<Shape> shapes = new List<Shape> {square1, rect1, cir1};
+
+        foreach(var shape in shapes)
+        {
+            string shapeName = shape.GetType().Name;
+            string shapeColor = shape.GetColor();
+            double shapeArea = shape.GetArea();
+            Console.WriteLine($"{shapeName} color - {shapeColor}");
+            Console.WriteLine($"{shapeName} area - {shapeArea}");
+        }
     }
 }
